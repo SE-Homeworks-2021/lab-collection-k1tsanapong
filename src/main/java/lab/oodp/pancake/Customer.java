40f21af7c04f5bd56e102dfa0a3b58851f53a573
Seki <6431503002@lamduan.mfu.ac.th>
2022-04-19 20:32:56 +0700

    Add peek

--- src/main/java/lab/oodp/pancake/Customer.java
+++ src/main/java/lab/oodp/pancake/Customer.java
@@ -45,29 +45,26 @@ public class Customer {
 
         int desiredMealSize = this.bellySize;
 
-        try {
-
             while (desiredMealSize > 0) {
-                //TODO: remove null, get pancake from stack
-                Pancake p = pancakes.pop();
-                if (p != null) {
 
+                //TODO: remove null, get pancake from stack
+
+                Pancake p = pancakes.peek();
+
+                if (p != null) {
+                    p = pancakes.pop();
                     System.out.println(getName() + " ate " + p + "!");
                     desiredMealSize--;
 
                 } //TODO: add else when p is null (no more pancake in the stack), and throws HungryException
 
+                else
+                {
+                    throw new HungryException(getName()+" is still hungry :(");
 
-
+                }
 
             }
-        }
-
-        catch (Exception e)
-        {
-            throw new HungryException(getName()+" is still hungry :(");
-        }
-
 
         System.out.println(getName() + " ate " + this.bellySize + " pancakes in total!");
 
